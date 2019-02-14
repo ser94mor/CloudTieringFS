f0ba9399921676d1824d3ca22823f791ea97d026
Sergey Morozov <ser94mor@gmail.com>
2019-02-14 21:51:05 +0300

    docs: Update Email Address
    Updated outdated author's email address sergey94morozov@gmail.com to the current one---sergey@morozov.ch.

--- inc/ops.h
+++ inc/ops.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2017  Sergey Morozov <sergey94morozov@gmail.com>
+ * Copyright (C) 2017  Sergey Morozov <sergey@morozov.ch>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
